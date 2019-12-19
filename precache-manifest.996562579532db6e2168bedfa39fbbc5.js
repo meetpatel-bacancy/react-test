3b40da59c53971cecb2adadb211ffc786cf15469
meetpatel-bacancy <meet.patel@bacancytechnology.com>
2019-12-19 10:41:32 +0530

    Updates

---
+++ precache-manifest.996562579532db6e2168bedfa39fbbc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff1a2244f5bbf17219a8faae32f6671",
+    "url": "/%7Breact-test%7D/index.html"
+  },
+  {
+    "revision": "0d7d47ec309a0bef306e",
+    "url": "/%7Breact-test%7D/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c4cce84cef59f00b7bc7",
+    "url": "/%7Breact-test%7D/static/js/2.5b8eb049.chunk.js"
+  },
+  {
+    "revision": "0d7d47ec309a0bef306e",
+    "url": "/%7Breact-test%7D/static/js/main.9e408323.chunk.js"
+  },
+  {
+    "revision": "c027610c31eed6be8295",
+    "url": "/%7Breact-test%7D/static/js/runtime-main.11cc792d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/%7Breact-test%7D/static/media/logo.25bf045c.svg"
+  }
+]);
